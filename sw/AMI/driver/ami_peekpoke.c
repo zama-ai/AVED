91fcadf3751a74ff66e6084d82634e1b56cdd95e
Pierre Gardrat <160511561+pgardratzama@users.noreply.github.com>
2025-02-25 17:28:37 +0100

    Update sw/AMI/driver/ami_peekpoke.c

--- sw/AMI/driver/ami_peekpoke.c
+++ sw/AMI/driver/ami_peekpoke.c
@@ -1,3 +1,9 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * ami_peekpoke.c - This file contains functions to read & write HPU registers
+ *
+ */
+
 #include <linux/types.h>
 #include <linux/delay.h>
 #include <linux/pci.h> 
